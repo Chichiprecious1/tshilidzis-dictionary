dbf6c610f61b5be8e1b2949131a83eb54a9e5a20
Chichiprecious1 <mulibanatshilidzi11@gmail.com>
2025-12-10 21:52:48 -0500

    added functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Hello</h1>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://www.shecodes.io/graduates/92144-tshilidzi-mulibana"
+              target="_blank"
+            >
+              {" "}
+              Tshilidzi Mulibana
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              href="https://github.com/Chichiprecious1/tshilidzis-dictionary"
+              target="_blank"
+            >
+              {" "}
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://tshilidzis-dictionary.netlify.app/"
+              target="_blank"
+            >
+              {" "}
+              Netlify
+            </a>
+            .
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
